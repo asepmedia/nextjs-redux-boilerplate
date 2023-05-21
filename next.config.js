461b928d34eac5f959dd7a78912d324e6a96bf1d
asepmedia <38859211+asepmedia@users.noreply.github.com>
2023-05-21 13:26:08 +0700

    feat: add sample services

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'd8c5gilsw3qw4.cloudfront.net',
+        port: '',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,13 @@
 import { getCourses } from '@/app/reducers/post/PostSlice';
 import { transformObjtoQuery } from '@/app/utils/helpers';
-import InputSelect from '@/components/molecules/InputSelect';
-import { Inter } from 'next/font/google';
+import { Nunito } from 'next/font/google';
 import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Nunito({ subsets: ['latin'] });
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -29,30 +29,36 @@ export default function Home() {
       <Head>
         <title>NextJS Boilerplate</title>
       </Head>
-      <div className={'flex flex-col'}>
-        <div className={'mx-auto   flex w-1/3 flex-col gap-y-4'}>
-          <div>
-            <Link href={'/blog/asep-yayat'}>
-              <h3>Test</h3>
-            </Link>
-            <InputSelect
-              htmlFor={'country_id'}
-              label={'Select Your Country'}
-              placeholder={'Choose one'}
-              loading={statePost.courses.loading}
-              options={statePost.courses.data.map((data) => {
-                return {
-                  value: data.id,
-                  text: data.name,
-                };
-              })}
-              onSelect={(value, option) => {
-                console.log({ value });
-                console.log({ option });
-                setPost(option.children);
-              }}
-            />
-            {post}
+      <div className={`flex flex-col pb-5 ${inter.className}`}>
+        <div className={'container mx-auto px-5'}>
+          <div className={'mt-5 pb-5'}>
+            <h3 className={'text-3xl font-bold text-cyan-800'}>Kursus Pilihan</h3>
+            <p className={'text-slate-400'}>Kurus terbaik untuk investasi masa depan</p>
+          </div>
+          <div className={'grid gap-5 lg:grid-cols-4'}>
+            {statePost.courses.data.length > 0
+              ? statePost.courses.data.map((post, index) => (
+                  <Link href={`/blog/${post.slug}`} key={index}>
+                    <div className={'flex min-h-[450px] flex-col justify-between gap-y-5 rounded-xl border px-6 py-3'}>
+                      <div className={'relative h-[250px]'}>
+                        <Image
+                          src={post.image_url}
+                          alt={''}
+                          fill
+                          style={{
+                            objectFit: 'contain',
+                          }}
+                        />
+                      </div>
+                      <h3 className={'text-2xl font-bold text-cyan-900'}>{post.name}</h3>
+                      <h3 className={'text-2xl font-black text-red-500'}>
+                        <s className={'mr-2 !font-medium !text-slate-600'}>{post.price_discount_formatted}</s>
+                        {post.price_formatted}
+                      </h3>
+                    </div>
+                  </Link>
+                ))
+              : null}
           </div>
         </div>
       </div>
